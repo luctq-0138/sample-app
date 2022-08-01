1151e99b717fe3aa4e5f6e2915c7c457db8cae27
luctq <tranquocluc610@gmail.com>
2022-08-01 11:19:34 +0700

    chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t(".signup_success")
+      redirect_to @user
+    else
+      flash[:danger] = t(".signup_fail")
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t(".user_not_found")
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit(:name, :email, :password, :password_confirmation)
+  end
 end
